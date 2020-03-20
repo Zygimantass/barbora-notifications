773aee10692296d9e9fc2513e186f9a965718848
Zygimantass <zygimantelis@gmail.com>
2020-03-20 18:44:01 +0200

    add: main.py now notifies an available time

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import logging
 import config
 
+from notifiers.print_notifier import PrintNotifier
 from barbora import Barbora
 from checker import Checker
 
@@ -9,9 +10,27 @@ logging.basicConfig(format="[%(levelname)s] [%(name)s] [%(asctime)s] - %(message
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+def get_print_notifier():
+    return PrintNotifier("Notification: {0}")
+
+def get_notifiers():
+    return [get_print_notifier()]
+
 def main():
     barbora_instance = Barbora(config.USERNAME, config.PASSWORD)
-    checker = Checker(barbora_instance, [])
-    checker.check_deliveries()
+
+    checker = Checker(barbora_instance, get_notifiers())
+    
+    delivery = checker.check_deliveries()
+
+    if not delivery:
+        logger.info("No deliveries found at this time")
+        return
+
+    d_time = delivery['deliveryTime'].replace("T", " ")
+    picking_time = delivery['pickingHour']
+
+    checker.notify("There is an available delivery time at {0}, the products are going to be picked at {1} hour".format(d_time, picking_time))
+
 if __name__ == "__main__":
     main()
